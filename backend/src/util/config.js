0e79878e288a8213e1555eaf4f22ae82e345c6e9
MrRanhkeal <120033960+MrRanhkeal@users.noreply.github.com>
2025-11-13 22:19:15 +0700

    commit

--- backend/src/util/config.js
+++ backend/src/util/config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     BAKONG: {
         BASE_URL: process.env.BAKONG_BASE_URL || 'https://api-bakong.nbc.gov.kh/v1',
-        ACCESS_TOKEN: process.env.BAKONG_ACCESS_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiZmNiNDBmMWZiZTJlNGM5OSJ9LCJpYXQiOjE3NTczNTYyNzgsImV4cCI6MTc2NTEzMjI3OH0.KqqN9MOdlnkQEcMPOasmvxgMHyNpmVyS3y-u4w9ufr4',
+        ACCESS_TOKEN: process.env.BAKONG_ACCESS_TOKEN || 'eyfgfdgsdfgfd.E3NTczNTYyNzgsImV4cCI6MTc2NTEzMjI3OH0.KqqN9MOdlnkQEcMPOasmvxgMHyNpmVyS3y-u4w9ufr4',
         ACCOUNT_ID: "ranh_keal@aclb",
         ACCOUNT_NAME: "Keal Ranh",
         CITY: "PHNOM PENH",
